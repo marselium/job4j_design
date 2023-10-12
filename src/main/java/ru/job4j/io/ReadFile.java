b3d9f3a702f255383176dcec616885b06550907e
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-10-12 13:49:11 +0300

    2.2.1. Ввод-вывод. 2.1. FileInputStream [#4898]

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -5,13 +5,13 @@ import java.io.IOException;
 
 public class ReadFile {
     public static void main(String[] args) {
-        try(FileInputStream in = new FileInputStream("data/input.txt")) {
-          StringBuilder txt = new StringBuilder();
-          int read;
-          while ((read = in.read()) != -1) {
-              txt.append((char) read);
-              System.out.println(txt);
-          }
+        try (FileInputStream in = new FileInputStream("data/input.txt")) {
+            StringBuilder txt = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                txt.append((char) read);
+                System.out.println(txt);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
