91a1e2b5cf13d3e969369e96b24c288478b18b90
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-10-23 12:01:56 +0300

    2.2.1. Ввод-вывод. 12. Валидация параметров запуска [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -5,11 +5,14 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
+        //Path start = Paths.get(".");
+        validate(args);
+        Path start = Paths.get(args[0]);
         search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
     }
 
@@ -21,5 +24,18 @@ public class Search {
             throw new RuntimeException(e);
         }
         return searcher.getPaths();
+    }
+
+    public static void validate(String[] args) {
+        Path path = Paths.get(args[0]);
+        if (args.length < 2) {
+            throw new IllegalArgumentException("There must be 2 arguments;");
+        }
+        if (!Files.exists(path) || !Files.isDirectory(path)) {
+            throw new IllegalArgumentException("Invalid path;");
+        }
+        if (!Objects.equals(args[1], ".js")) {
+            throw new IllegalArgumentException("Format must be .js");
+        }
     }
 }
