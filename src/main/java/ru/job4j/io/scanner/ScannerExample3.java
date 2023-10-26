7475b1192c91de53181c8a7b528e09d59cbe8b35
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-10-26 14:38:59 +0300

    2.2.1. Ввод-вывод. 18. Класс Console [#504991]

---
+++ src/main/java/ru/job4j/io/scanner/ScannerExample3.java
@@ -0,0 +1,23 @@
+package ru.job4j.io.scanner;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FilterOutputStream;
+import java.util.Scanner;
+
+public class ScannerExample3 {
+    public static void main(String[] args) throws Exception {
+        var data = "A 1B FF 100";
+        var file = File.createTempFile("data", null);
+        try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
+            out.write(data.getBytes());
+        }
+        try (var scanner = new Scanner(file).useRadix(16)) {
+            while (scanner.hasNextInt()) {
+                System.out.println(scanner.nextInt());
+                System.out.println(" ");
+            }
+        }
+    }
+}
