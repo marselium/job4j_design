d150512856ab749c67142ef85f587277c76bdad0
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-11-06 03:00:21 +0300

    2.2.4. Сериализация 5. Преобразование JSON в POJO. JsonObject [#315064]

--- src/main/java/ru/job4j/serialization/json/Person.java
+++ src/main/java/ru/job4j/serialization/json/Person.java
@@ -1,8 +1,12 @@
 package ru.job4j.serialization.json;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import ru.job4j.serialization.json.Contact;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Person {
     private final boolean sex;
@@ -10,11 +14,19 @@ public class Person {
     private final Contact contact;
     private final String[] statuses;
 
-    public Person(boolean sex, int age, Contact contact, String[] statuses) {
+    public Person(boolean sex, int age, Contact contact, String... statuses) {
         this.sex = sex;
         this.age = age;
         this.contact = contact;
         this.statuses = statuses;
+    }
+
+    private int getAge() {
+        return age;
+    }
+
+    private boolean getSex() {
+        return sex;
     }
 
     @Override
@@ -25,5 +37,31 @@ public class Person {
                 + ", contact=" + contact
                 + ", statuses=" + Arrays.toString(statuses)
                 + '}';
+    }
+
+    public static void main(String[] args) {
+
+        /* JSONObject из json-строки строки */
+        JSONObject jsonContact = new JSONObject("{\"phone\":\"+7(924)111-111-11-11\"}");
+
+        /* JSONArray из ArrayList */
+        List<String> list = new ArrayList<>();
+        list.add("Student");
+        list.add("Free");
+        JSONArray jsonStatuses = new JSONArray(list);
+
+        /* JSONObject напрямую методом put */
+        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("sex", person.getSex());
+        jsonObject.put("age", person.getAge());
+        jsonObject.put("contact", jsonContact);
+        jsonObject.put("statuses", jsonStatuses);
+
+        /* Выведем результат в консоль */
+        System.out.println(jsonObject.toString());
+
+        /* Преобразуем объект person в json-строку */
+        System.out.println(new JSONObject(person).toString());
     }
 }
--- src/main/java/ru/job4j/serialization/json/Travel.java
+++ src/main/java/ru/job4j/serialization/json/Travel.java
@@ -1,10 +1,13 @@
 package ru.job4j.serialization.json;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import ru.job4j.serialization.json.Country;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class Travel {
 
@@ -22,6 +25,18 @@ public class Travel {
         this.hotel = hotel;
     }
 
+    private boolean getNeedVisa() {
+        return needVisa;
+    }
+
+    private int getCost() {
+        return cost;
+    }
+
+    private String getTourOperator() {
+        return tourOperator;
+    }
+
     @Override
     public String toString() {
         return "Person{"
@@ -31,5 +46,24 @@ public class Travel {
                 + ", country=" + country
                 + ", hotel=" + Arrays.toString(hotel)
                 + '}';
+    }
+
+    public static void main(String[] args) {
+        JSONObject jsonCountry = new JSONObject("{\"name\":\"Egypt\"}");
+        List<String> list = new ArrayList<>();
+        list.add("Kleopatra");
+        list.add("5-star");
+        JSONArray jsonHotel = new JSONArray(list);
+
+        final Travel travel = new Travel(true, 122000, "Anex Tour",
+                new Country("Egypt"), new String[]{"Kleopatra", "5-Star"});
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("needVisa", travel.getNeedVisa());
+        jsonObject.put("cost", travel.getCost());
+        jsonObject.put("tourOperator", travel.getTourOperator());
+        jsonObject.put("country", jsonCountry);
+        jsonObject.put("hotel", jsonHotel);
+        System.out.println(jsonObject.toString());
+        System.out.println(new JSONObject(travel).toString());
     }
 }
