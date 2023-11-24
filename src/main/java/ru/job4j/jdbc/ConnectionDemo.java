6b9d95196fcce8b4303736441a6a51867b867b8d
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-11-24 18:13:13 +0300

    2.3.5. JDBC. 0. JDBC

--- src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -12,7 +12,7 @@ import java.util.Properties;
 public class ConnectionDemo {
     public static void main(String[] args) throws SQLException, IOException {
         final Properties properties = new Properties();
-        properties.load(new FileInputStream(new File("data/app.properties")));
+        properties.load(new FileInputStream("data/app.properties"));
         String url = properties.getProperty("datasource.url");
         String login = properties.getProperty("datasource.username");
         String password = properties.getProperty("datasource.password");
