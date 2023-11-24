06a3520e11cb8aac692bec37a400e9c2d9c28b27
Marsel Yumagulov <marseliumagulov@yandex.ru>
2023-11-24 18:09:25 +0300

    2.3.5. JDBC. 0. JDBC

---
+++ src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -0,0 +1,25 @@
+package ru.job4j.jdbc;
+
+import ru.job4j.iterator.io.Config;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionDemo {
+    public static void main(String[] args) throws SQLException, IOException {
+        final Properties properties = new Properties();
+        properties.load(new FileInputStream(new File("data/app.properties")));
+        String url = properties.getProperty("datasource.url");
+        String login = properties.getProperty("datasource.username");
+        String password = properties.getProperty("datasource.password");
+        try (Connection connection = DriverManager.getConnection(url, login, password)) {
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getUserName());
+            System.out.println(metaData.getURL());
+        }
+    }
+}
